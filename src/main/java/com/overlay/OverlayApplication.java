105743c6e42fa1edb4f4be8bdc5ddea94fa1707a
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-12-20 18:33:27 -0300

    Adiciona a base das dependências Maven do projeto

---
+++ src/main/java/com/overlay/OverlayApplication.java
@@ -0,0 +1,13 @@
+package com.overlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverlayApplication.class, args);
+	}
+
+}
