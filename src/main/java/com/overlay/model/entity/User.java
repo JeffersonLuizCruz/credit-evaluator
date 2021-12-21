ea0c2b0366fd0512a1c481bcacd046c936c12e8e
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-12-20 21:26:16 -0300

    Adiciona entidade user

---
+++ src/main/java/com/overlay/model/entity/User.java
@@ -0,0 +1,30 @@
+package com.overlay.model.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+}
---
+++ src/main/java/com/overlay/security/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.overlay.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
