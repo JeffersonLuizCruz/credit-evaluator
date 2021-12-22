6e48ec7fedf2bf49fae194126ec6094079157b5f
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-12-21 22:37:04 -0300

    Adiciona authenticationManager como Bean

--- src/main/java/com/overlay/controller/UserController.java
+++ src/main/java/com/overlay/controller/UserController.java
@@ -1,5 +1,12 @@
 package com.overlay.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+
 public class UserController {
+	
+	@Autowired AuthenticationManager authManager;
+	
+	
 
 }
