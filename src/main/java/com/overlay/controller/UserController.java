2bef435f45804e4675e5d84b38a389123801b3d4
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-12-22 00:14:46 -0300

    Adiciona

--- src/main/java/com/overlay/controller/UserController.java
+++ src/main/java/com/overlay/controller/UserController.java
@@ -3,10 +3,14 @@ package com.overlay.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 
+import com.overlay.security.JwtManager;
+import com.overlay.service.UserService;
+
 public class UserController {
 	
 	@Autowired AuthenticationManager authManager;
-	
+	@Autowired private UserService userService;
+	@Autowired private JwtManager jwtManager;
 	
 
 }
