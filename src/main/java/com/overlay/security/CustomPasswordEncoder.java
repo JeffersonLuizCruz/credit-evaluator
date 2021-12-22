491c647dc919b200f4a5871df614ed36f129c1d5
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-12-21 22:01:32 -0300

    Adiciona hash para password

--- src/main/java/com/overlay/security/CustomPasswordEncoder.java
+++ src/main/java/com/overlay/security/CustomPasswordEncoder.java
@@ -2,16 +2,20 @@ package com.overlay.security;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.overlay.util.HashUtil;
+
 public class CustomPasswordEncoder implements PasswordEncoder{
 
 	@Override
 	public String encode(CharSequence rawPassword) {
-		return null;
+		String hash = HashUtil.getSecureHash(rawPassword.toString());
+		return hash;
 	}
 
 	@Override
 	public boolean matches(CharSequence rawPassword, String encodedPassword) {
-		return false;
+		String hash = HashUtil.getSecureHash(rawPassword.toString());
+		return hash.equals(encodedPassword);
 	}
 
 }
