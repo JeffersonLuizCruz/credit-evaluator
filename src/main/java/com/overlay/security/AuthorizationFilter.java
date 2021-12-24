e75b4e23dd4d0508aab1ec906f41593e57e240fe
Jefferson <jefferson.luiz.cruz@gmail.com>
2021-12-24 15:36:59 -0300

    Adiciona exception AccessDeniedException

--- src/main/java/com/overlay/security/AuthorizationFilter.java
+++ src/main/java/com/overlay/security/AuthorizationFilter.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.FilterChain;
--- src/main/java/com/overlay/security/SecurityConfig.java
+++ src/main/java/com/overlay/security/SecurityConfig.java
@@ -6,13 +6,17 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.overlay.service.impl.UserServiceImpl;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired CustomPasswordEncoder customPasswordEncoder;
@@ -27,6 +31,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring()
 			.antMatchers(HttpMethod.POST, "/users/login");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+				   .authorizeRequests()
+				   .anyRequest().authenticated();
+		
+		http.addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
